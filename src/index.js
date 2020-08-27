ee856ab8f2d2bdb725892221b2c259379fb383c0
realstevelee <realstevelee@gmail.com>
2020-08-27 18:56:46 -0400

    1st commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from "react-dom";
+import SeasonDisplay from './SeasonDisplay'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errMsg: '' };  
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errMsg: err.message })
+        );
+    }
+
+    render() {
+        if(this.state.errMsg && !this.state.lat) {
+        return <div>Error: { this.state.errMsg }</div>
+        }
+        
+        if(!this.state.errMsg && this.state.lat) {
+        return <SeasonDisplay lat = { this.state.lat }/>
+        }
+
+        return <div>Loading!</div>
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
